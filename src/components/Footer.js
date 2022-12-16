b7f247b7e266acd59f34a37915251a70bdd4c045
cesarcuadro <cesarcuadro777@gmail.com>
2022-12-16 11:58:14 -0800

    passing off to ramirez

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        
+          active
+          href="#"
+        >
+          Link
+        <>
+         href="#">
+          Another Link
+        <>
+        
+          disabled
+          href="#"
+        >
+          Disabled Link
+
+    </div>
+  )
+}
+
+export default Footer

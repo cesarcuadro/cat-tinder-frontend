651ee94cdd3d7b12de80714ead35b913477087c1
cesarcuadro <106943802+cesarcuadro@users.noreply.github.com>
2023-01-04 14:22:53 -0800

    Update CatShow.test.js

--- src/pages/CatShow.test.js
+++ src/pages/CatShow.test.js
@@ -1,6 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import CatShow from "./CatShow";
 import mockCats from "../mockCats";
+import { MemoryRouter, Routes, Route } from 'react-router-dom'
+
+    const renderComponent = () => {
+      render(
+      <MemoryRouter initialEntries={["/catshow/1"]}>
+        <Routes>
+            <Route path="catshow/:id" element={<CatShow cats={mockCats} />}/>
+        </Routes>
+      </MemoryRouter>
+      )
+    }
 
 describe("<CatShow />", () => {
   it("renders a card for each cat", () => {

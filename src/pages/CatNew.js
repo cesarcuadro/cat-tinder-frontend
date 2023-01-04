b54f4438c746dc74a4cfdbaf6c0d1679af349ee2
Shadow090509 <jamoso090509@gmail.com>
2023-01-04 10:05:21 -0800

    push

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 const CatNew = () => {
-  return (
-    <div>CatNew</div>
-  )
-}
+  const [newCat, setNewCat] = useState({
+    name: "",
+    age: "",
+    enjoys: "",
+    image: "",
+  });
 
-export default CatNew
+  const handleChange = (e) => {
+    setNewCat({ ...newCat, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <>
+      <Form>
+        <FormGroup>
+          <Label for="exampleName">Name</Label>
+          <Input
+            // id="name"
+            name="name"
+            onChange={handleChange}
+            placeholder="What is your name?"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleName">Age</Label>
+          <Input
+            // id="name"
+            name="age"
+            onChange={handleChange}
+            placeholder="What is your age?"
+            type="number"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleEnjoy">Enjoys</Label>
+          <Input
+            // id="name"
+            name="Enjoys"
+            onChange={handleChange}
+            placeholder="What do you enjoy?"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleImage">Selfie</Label>
+          <Input
+            // id="name"
+            name="image"
+            onChange={handleChange}
+            placeholder="post a Selfie?"
+            type="url"
+          />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    </>
+  );
+};
+
+export default CatNew;

42f57daf23f3055490c9fd874c9086875f457c80
cesarcuadro <cesarcuadro777@gmail.com>
2023-01-28 09:51:32 -0800

    completed update functionality

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,46 +1,41 @@
 import React from "react";
-import {
-  Card,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button,
-} from "reactstrap";
+import { Card, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import "../App.css";
 
 const CatIndex = ({ cats }) => {
   return (
     <div className="body">
-    <main>
-      {cats?.map((cat, index) => {
-        return (
-          <>
-          <div className="card-index-cards">
-            <Card
-              color="light"
-              style={{
-                width: "18rem",
-                marginLeft: "auto",
-                marginRight: "auto"
-              }}
-            >
-              <img alt="Sample" src={cat.image} />
-              <CardBody>
-                <CardTitle tag="h5">{cat.name}</CardTitle>
-                <CardSubtitle className="mb-2 text-muted" tag="h6">
-                  {cat.age}
-                </CardSubtitle>
-                <NavLink to={`/catshow/${cat.id}`}>
-                  <Button>Sniff</Button>
-                </NavLink>
-              </CardBody>
-            </Card>
-            </div>
-          </>
-        );
-      })}
-    </main>
+      <main>
+        {cats?.map((cat, index) => {
+          return (
+            <>
+              <div className="card-index-cards">
+                <Card
+                  color="light"
+                  style={{
+                    width: "18rem",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                  }}
+                  key={index}
+                >
+                  <img alt="Sample" src={cat.image} />
+                  <CardBody>
+                    <CardTitle tag="h5">{cat.name}</CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">
+                      {cat.age}
+                    </CardSubtitle>
+                    <NavLink to={`/catshow/${cat.id}`}>
+                      <Button>Sniff</Button>
+                    </NavLink>
+                  </CardBody>
+                </Card>
+              </div>
+            </>
+          );
+        })}
+      </main>
     </div>
   );
 };
